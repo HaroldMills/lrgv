f30e3b8c65bbf3d681e50a2220fa0092d46579c8
Harold Mills <harold.mills@gmail.com>
2025-08-07 12:30:41 -0400

    Add recordings to detection simulator.

--- lrgv/scripts/simulate_detection_lrgv.py
+++ lrgv/scripts/simulate_detection_lrgv.py
@@ -5,7 +5,7 @@ import time
 
 
 # The source, active, and retired test data directory hierarchies parallel
-# the laptop data directory hierarchy and look like:
+# the station laptop data directory hierarchy and look like:
 #
 #     <root dir>
 #         <station name>
@@ -14,10 +14,15 @@ import time
 #                     Incoming
 #                     Created
 #                     Archived
+#             Recordings
+#                 <recorder name>
+#                     Incoming
+#                     Archived
 #
 # For a given station, Old Bird detector clips appear in the station
 # directory <station name>, while other detector clips appear in the
-# `Incoming` directory.
+# detectors' `Incoming` directories. Recording files appear in the
+# recorders' `Incoming` directories.
 
 
 PROJECT_NAME = 'LRGV'
@@ -34,6 +39,8 @@ ACTIVE_DATA_DIR_PATH = DATA_DIR_PATH / 'Active'
 RETIRED_DATA_DIR_PATH = DATA_DIR_PATH / 'Retired'
 
 DATA_DIR_PATHS = (ACTIVE_DATA_DIR_PATH, RETIRED_DATA_DIR_PATH)
+
+RECORDING_DIR_NAME = 'Recordings'
 
 CLIP_DIR_NAME = 'Clips'
 
@@ -55,6 +62,8 @@ ALL_DETECTOR_NAMES = ('Dick', 'Nighthawk')
 
 SIMULATION_STATION_NAMES = ('Alamo', 'Port Isabel')
 
+SIMULATION_RECORDER_NAMES = ('Vesper Recorder',)
+
 SIMULATION_DETECTOR_NAMES = ('Dick', 'Nighthawk')
 
 DETECTION_SLEEP_PERIOD = 2
@@ -67,25 +76,27 @@ chain = itertools.chain.from_iterable
 
 def main():
 
-    print(f'Clearing test data directories...')
-    clear_dirs()
+    print(f'Clearing test data directory...')
+    clear_dir(DATA_DIR_PATH)
+    # clear_dirs()
 
-    clip_count = 0
+    simulate_recording()
+    simulate_detection()
 
-    for audio_file_path in get_clip_audio_file_paths():
 
-        if MAX_CLIP_COUNT is not None and clip_count == MAX_CLIP_COUNT:
-            break
+def clear_dir(dir_path):
 
-        relative_path = audio_file_path.relative_to(DATA_SOURCE_DIR_PATH)
-        print(f'Detected "{relative_path}...')
+    if dir_path.exists():
 
-        detect_clip(audio_file_path)
+        for path in dir_path.glob('*'):
 
-        clip_count += 1
+            if path.is_file():
+                path.unlink()
 
-        time.sleep(DETECTION_SLEEP_PERIOD)
-        
+            elif path.is_dir():
+                clear_dir(path) 
+                path.rmdir()
+
 
 def clear_dirs():
 
@@ -119,6 +130,61 @@ def get_station_dir_name(station_name):
     return f'{PROJECT_NAME} - {station_name}'
 
 
+def simulate_recording():
+
+    for file_path in get_recording_metadata_file_paths():
+
+        relative_path = file_path.relative_to(DATA_SOURCE_DIR_PATH)
+        print(f'Recorded "{relative_path}"...')
+
+        copy_recording_metadata_file(file_path)
+
+
+def get_recording_metadata_file_paths():
+
+    def get_file_paths(station_name, recorder_name):
+        station_dir_name = get_station_dir_name(station_name)
+        dir_path = \
+            DATA_SOURCE_DIR_PATH / station_dir_name / RECORDING_DIR_NAME / \
+            recorder_name / INCOMING_DIR_NAME
+        return sorted(p for p in dir_path.glob('*.json'))
+
+    cases = \
+        itertools.product(SIMULATION_STATION_NAMES, SIMULATION_RECORDER_NAMES)
+    
+    return chain(get_file_paths(s, d) for s, d in cases)
+
+
+def copy_recording_metadata_file(file_path):
+    relative_path = file_path.relative_to(DATA_SOURCE_DIR_PATH)
+    to_dir_path = ACTIVE_DATA_DIR_PATH / relative_path.parent
+    copy_file(file_path, to_dir_path)
+
+
+def copy_file(file_path, to_dir_path):
+    to_dir_path.mkdir(mode=0o755, parents=True, exist_ok=True)
+    shutil.copy2(file_path, to_dir_path)
+
+
+def simulate_detection():
+
+    clip_count = 0
+
+    for audio_file_path in get_clip_audio_file_paths():
+
+        if MAX_CLIP_COUNT is not None and clip_count == MAX_CLIP_COUNT:
+            break
+
+        relative_path = audio_file_path.relative_to(DATA_SOURCE_DIR_PATH)
+        print(f'Detected "{relative_path}"...')
+
+        copy_clip_files(audio_file_path)
+
+        clip_count += 1
+
+        time.sleep(DETECTION_SLEEP_PERIOD)
+        
+
 def get_clip_audio_file_paths():
     old_bird_paths = get_old_bird_clip_audio_file_paths()
     other_paths = get_other_clip_audio_file_paths()
@@ -129,7 +195,7 @@ def get_clip_audio_file_paths():
 
 def get_old_bird_clip_audio_file_paths():
 
-    def get_clips_aux(station_name):
+    def get_file_paths(station_name):
         station_dir_name = get_station_dir_name(station_name)
         dir_path = DATA_SOURCE_DIR_PATH / station_dir_name
         if not dir_path.exists():
@@ -137,12 +203,12 @@ def get_old_bird_clip_audio_file_paths():
                 f'Station data source directory "{dir_path}" does not exist.')
         return sorted(p for p in dir_path.glob('*.wav'))
     
-    return [get_clips_aux(n) for n in SIMULATION_STATION_NAMES]
+    return [get_file_paths(n) for n in SIMULATION_STATION_NAMES]
 
 
 def get_other_clip_audio_file_paths():
 
-    def get_clips_aux(station_name, detector_name):
+    def get_file_paths(station_name, detector_name):
         station_dir_name = get_station_dir_name(station_name)
         dir_path = \
             DATA_SOURCE_DIR_PATH / station_dir_name / CLIP_DIR_NAME / \
@@ -152,19 +218,19 @@ def get_other_clip_audio_file_paths():
     cases = \
         itertools.product(SIMULATION_STATION_NAMES, SIMULATION_DETECTOR_NAMES)
     
-    return [get_clips_aux(s, d) for s, d in cases]
+    return [get_file_paths(s, d) for s, d in cases]
 
 
-def detect_clip(audio_file_path):
+def copy_clip_files(audio_file_path):
 
     relative_path = audio_file_path.relative_to(DATA_SOURCE_DIR_PATH)
     to_dir_path = ACTIVE_DATA_DIR_PATH / relative_path.parent
 
-    shutil.copy2(audio_file_path, to_dir_path)
+    copy_file(audio_file_path, to_dir_path)
 
     metadata_file_path = audio_file_path.with_suffix('.json')
     if metadata_file_path.exists():
-        shutil.copy2(metadata_file_path, to_dir_path)
+        copy_file(metadata_file_path, to_dir_path)
 
 
 if __name__ == '__main__':
